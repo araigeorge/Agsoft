60cc83f6c9e48900a86eed49f3b78023be969f4e
araigeorge <araigeorge@gmail.com>
2024-08-24 13:36:01 -0400

    creacion basica del proyecto

---
+++ Agsoft/Agsoft.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Agsoft.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Agsoft/Agsoft.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Agsoft.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
