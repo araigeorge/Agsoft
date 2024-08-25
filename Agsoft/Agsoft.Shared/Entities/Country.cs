9051b579feb1e9cb2ee6564913d94b2ca9c66fdb
araigeorge <araigeorge@gmail.com>
2024-08-24 20:16:11 -0400

    Crear Base de datos con la prumera entidad Country

---
+++ Agsoft/Agsoft.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agsoft.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage ="El campo país no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage ="El campo {0} es requerido.")]
+        public string Name { get; set; } = null!;
+      
+    }
+}
